b812c23a1707a729d4d1cf2b80e729fa239682d1
Lucy Laurentino <9998329@bcs.net.nz>
2017-08-14 10:30:09 +1200

    inicial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _9998329_task01_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
